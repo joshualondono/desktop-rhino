0b7dfad58712f599bc5f8e5261ff2c9ec2db1a85
joshualondono <joshualondono@outlook.com>
2016-04-07 00:17:29 -0700

    Added files via upload

---
+++ phpnode.js
@@ -0,0 +1,75 @@
+const execFile = require('child_process').execFile; 
+const exec = require('child_process').exec;
+var portfinder = require('portfinder');
+
+exports.macserver = function () { 
+
+if(process.argv[2]) {
+  var port = process.argv[2];
+  if ( isNaN(port) ) {
+    console.log('passed value is not a port');
+  } else {
+    portfinder.basePort = port;
+  }
+}
+portfinder.getPort(function(err, port) {
+  if(err) {
+    console.error(err);
+  } else {
+    console.log(port);  var a = execFile('bin/nwjs', ['-S','localhost:' + port]); 
+
+
+process.on('exit', function () {
+    a.kill();
+
+
+});
+
+}
+});
+}
+exports.macinternal = function () {  
+
+if(process.argv[2]) {
+  var port = process.argv[2];
+  if ( isNaN(port) ) {
+    console.log('passed value is not a port');
+  } else {
+    portfinder.basePort = port;
+  }
+}
+portfinder.getPort(function(err, port) {
+  if(err) {
+    console.error(err);
+  } else {
+    console.log(port); var a = exec('php -S localhost:' + port); 
+
+
+process.on('exit', function () {
+    a.kill();
+
+
+});
+
+}
+});
+}
+exports.winserver = function () { const execFile = require('child_process').execFile; var fs = require("fs"); var portfinder = require('portfinder');
+
+if(process.argv[2]) {
+  var port = process.argv[2];
+  if ( isNaN(port) ) {
+    console.log('passed value is not a port');
+  } else {
+    portfinder.basePort = port;
+  }
+}
+portfinder.getPort(function(err, port) {
+  if(err) {
+    console.error(err);
+  } else {
+    console.log(port); const child = execFile('bin/php', ['-S','localhost:' + port]); 
+
+}
+});
+}
